c831e04de871784281d83d0359401d3cea03279e
Paige Moore <moorepaige001@gmail.com>
2023-06-08 15:38:49 -0700

    saving progress

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,25 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          alt="Profile Pic"
+          ></img>
       </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Paige Moore</div>
+        <div className="brief_description">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+        
     )
   }
 }
